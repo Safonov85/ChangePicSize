7bf55b602aa7a585bca0d19c4278e686f7afdbd8
safonov <safonov1985@protonmail.com>
2020-06-30 22:17:55 +0200

    First Commit

---
+++ ChangePicSize/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace ChangePicSize
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoadPicButton = new System.Windows.Forms.Button();
+            this.ResizePicturesButton = new System.Windows.Forms.Button();
+            this.ResizeLevelCombobox = new System.Windows.Forms.ComboBox();
+            this.ResizeToLabel = new System.Windows.Forms.Label();
+            this.PicturesListbox = new System.Windows.Forms.ListBox();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.AboutButton = new System.Windows.Forms.Button();
+            this.QualityTextBox = new System.Windows.Forms.TextBox();
+            this.QualityLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // LoadPicButton
+            // 
+            this.LoadPicButton.Location = new System.Drawing.Point(28, 29);
+            this.LoadPicButton.Name = "LoadPicButton";
+            this.LoadPicButton.Size = new System.Drawing.Size(164, 81);
+            this.LoadPicButton.TabIndex = 0;
+            this.LoadPicButton.Text = "Load Picture(s)";
+            this.LoadPicButton.UseVisualStyleBackColor = true;
+            this.LoadPicButton.Click += new System.EventHandler(this.LoadPicButton_Click);
+            // 
+            // ResizePicturesButton
+            // 
+            this.ResizePicturesButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ResizePicturesButton.Location = new System.Drawing.Point(28, 129);
+            this.ResizePicturesButton.Name = "ResizePicturesButton";
+            this.ResizePicturesButton.Size = new System.Drawing.Size(164, 100);
+            this.ResizePicturesButton.TabIndex = 1;
+            this.ResizePicturesButton.Text = "RESIZE";
+            this.ResizePicturesButton.UseVisualStyleBackColor = false;
+            this.ResizePicturesButton.Click += new System.EventHandler(this.ResizePicturesButton_Click);
+            // 
+            // ResizeLevelCombobox
+            // 
+            this.ResizeLevelCombobox.FormattingEnabled = true;
+            this.ResizeLevelCombobox.Items.AddRange(new object[] {
+            "Small",
+            "Medium",
+            "Large"});
+            this.ResizeLevelCombobox.Location = new System.Drawing.Point(16, 297);
+            this.ResizeLevelCombobox.Name = "ResizeLevelCombobox";
+            this.ResizeLevelCombobox.Size = new System.Drawing.Size(164, 28);
+            this.ResizeLevelCombobox.TabIndex = 1;
+            // 
+            // ResizeToLabel
+            // 
+            this.ResizeToLabel.AutoSize = true;
+            this.ResizeToLabel.Location = new System.Drawing.Point(12, 342);
+            this.ResizeToLabel.Name = "ResizeToLabel";
+            this.ResizeToLabel.Size = new System.Drawing.Size(84, 20);
+            this.ResizeToLabel.TabIndex = 3;
+            this.ResizeToLabel.Text = "Resize To:";
+            // 
+            // PicturesListbox
+            // 
+            this.PicturesListbox.FormattingEnabled = true;
+            this.PicturesListbox.ItemHeight = 20;
+            this.PicturesListbox.Location = new System.Drawing.Point(270, 41);
+            this.PicturesListbox.Name = "PicturesListbox";
+            this.PicturesListbox.Size = new System.Drawing.Size(420, 284);
+            this.PicturesListbox.TabIndex = 4;
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.BackColor = System.Drawing.Color.MistyRose;
+            this.CancelButton.Location = new System.Drawing.Point(285, 365);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(227, 108);
+            this.CancelButton.TabIndex = 5;
+            this.CancelButton.Text = "CANCEL (not working yet)";
+            this.CancelButton.UseVisualStyleBackColor = false;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // AboutButton
+            // 
+            this.AboutButton.BackColor = System.Drawing.SystemColors.Menu;
+            this.AboutButton.Location = new System.Drawing.Point(12, 466);
+            this.AboutButton.Name = "AboutButton";
+            this.AboutButton.Size = new System.Drawing.Size(80, 48);
+            this.AboutButton.TabIndex = 6;
+            this.AboutButton.Text = "About";
+            this.AboutButton.UseVisualStyleBackColor = false;
+            this.AboutButton.Click += new System.EventHandler(this.AboutButton_Click);
+            // 
+            // QualityTextBox
+            // 
+            this.QualityTextBox.Location = new System.Drawing.Point(98, 250);
+            this.QualityTextBox.Name = "QualityTextBox";
+            this.QualityTextBox.Size = new System.Drawing.Size(68, 26);
+            this.QualityTextBox.TabIndex = 7;
+            this.QualityTextBox.Text = "100";
+            // 
+            // QualityLabel
+            // 
+            this.QualityLabel.AutoSize = true;
+            this.QualityLabel.Location = new System.Drawing.Point(31, 253);
+            this.QualityLabel.Name = "QualityLabel";
+            this.QualityLabel.Size = new System.Drawing.Size(61, 20);
+            this.QualityLabel.TabIndex = 8;
+            this.QualityLabel.Text = "Quality:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(727, 526);
+            this.Controls.Add(this.QualityLabel);
+            this.Controls.Add(this.QualityTextBox);
+            this.Controls.Add(this.AboutButton);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.PicturesListbox);
+            this.Controls.Add(this.ResizeToLabel);
+            this.Controls.Add(this.ResizeLevelCombobox);
+            this.Controls.Add(this.ResizePicturesButton);
+            this.Controls.Add(this.LoadPicButton);
+            this.Name = "Form1";
+            this.Text = "Re-size Picture";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button LoadPicButton;
+        private System.Windows.Forms.Button ResizePicturesButton;
+        private System.Windows.Forms.ComboBox ResizeLevelCombobox;
+        private System.Windows.Forms.Label ResizeToLabel;
+        private System.Windows.Forms.ListBox PicturesListbox;
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.Button AboutButton;
+        private System.Windows.Forms.TextBox QualityTextBox;
+        private System.Windows.Forms.Label QualityLabel;
+    }
+}
+
---
+++ ChangePicSize/Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChangePicSize
+{
+    public partial class Form1 : Form
+    {
+        int item = 0;
+        List<Image> pictureList = new List<Image>();
+        string pathSave;
+        Point Large = new Point(1280, 1024);
+
+        public Form1()
+        {
+            InitializeComponent();
+            ResizeLevelCombobox.SelectedItem = 1;
+            ResizeToLabel.Text = "Resize To: " + Large.X +" x " + Large.Y;
+        }
+
+        private void LoadPicButton_Click(object sender, EventArgs e)
+        {
+            
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image Files(*.jpg;)|*.jpg;";
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            dialog.Multiselect = true;
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if(pictureList.Count > 0)
+                    {
+                        pictureList.Clear();
+                    }
+                    pathSave = Path.GetDirectoryName(dialog.FileName);
+                    int item = 0;
+
+                    // ACTUAL FILES ADDED TO IMAGE LIST
+                    foreach (String file in dialog.FileNames)
+                    {
+                        pictureList.Add(Image.FromFile(dialog.FileNames[item]));
+                        //pictureList.Add()
+                        item++;
+                    }
+
+                    item = 0;
+
+                    // Just names added to listBox (simple strings)
+                    foreach (String file in dialog.SafeFileNames)
+                    {
+                        PicturesListbox.Items.Add(dialog.SafeFileNames[item]);
+                        item++;
+                    }
+                }
+                catch (Exception expt)
+                {
+                    MessageBox.Show("An error occured: " + expt.Message);
+                }
+                Console.WriteLine("done");
+            }
+
+            //if (dialog.ShowDialog() == DialogResult.OK)
+            //{
+            //    try
+            //    {
+            //        int item = 0;
+            //        foreach (String file in dialog.SafeFileNames)
+            //        {
+            //            PicturesListbox.Items.Add(dialog.SafeFileNames[item] + "✔");
+            //            item++;
+            //        }
+            //    }
+            //    catch(Exception expt)
+            //    {
+            //        MessageBox.Show("An error occured: " + expt.Message);
+            //    }
+
+            //}
+            //item++;
+            //PicturesListbox.Items.Add("Item num" + item);
+
+        }
+
+        private void FilterFiles(OpenFileDialog dialog)
+        {
+            //dialog.Filter =
+            //"Images (*.BMP;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF|" +
+            //"All files (*.*)|*.*";
+        }
+
+        private void ResizePicturesButton_Click(object sender, EventArgs e)
+        {
+            if(pictureList.Count == 0)
+            {
+                MessageBox.Show("No files loaded in the list");
+                return;
+            }
+
+            try
+            {
+                //if(!Directory.Exists(pathSave + "\\mindre"))
+                //{
+                //    Directory.CreateDirectory(pathSave + "\\mindre");
+                //}
+                
+                int count = 0;
+
+                //ImageCodecInfo jpgEncoder = GetEncoder(ImageFormat.Jpeg);
+                //System.Drawing.Imaging.Encoder myEncoder =
+                //    System.Drawing.Imaging.Encoder.Quality;
+                //EncoderParameters myEncoderParameters = new EncoderParameters(1);
+
+                //EncoderParameter myEncoderParameter = new EncoderParameter(myEncoder, QualityTextBox.Text);
+                //myEncoderParameters.Param[0] = myEncoderParameter;
+
+                this.Text = "Re-size Picture (CONVERTING)";
+
+                // SAVING resized images to new 'mindre' folder !!!!!!!!!!!!!!
+                foreach (var picture in pictureList)
+                {
+                    Image newImage;
+                    ResizePicture resizePic = new ResizePicture();
+                    newImage = resizePic.ResizeImage(picture, Large.X, Large.Y);
+                    string pathExists = pathSave + "\\mindre\\newpic" + count + ".jpg";
+
+                    //if(pathExists != "blah")
+                    //{
+                    //    MessageBox.Show("blah is not the path");
+                    //    return;
+                    //}
+
+                    //if (Directory.Exists(pathExists) != true)
+                    //{
+                    //    MessageBox.Show("The file with the same name exists. Operation will end. Remove the file and try again(restart application recommended). File: " + "newpic" + count + ".jpg");
+                    //    return;
+                    //}
+
+                    newImage.Save(pathSave + "\\mindre\\newpic" + count + ".jpg", ImageFormat.Jpeg);
+                    newImage.Dispose();
+                    PicturesListbox.Items[count] = PicturesListbox.Items[count] + " ✔";
+                    count++;
+                }
+
+                this.Text = "Re-size Picture";
+
+
+            }
+            catch(Exception expt)
+            {
+                MessageBox.Show(expt.Message);
+            }
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AboutButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Made by Mikhail Safonov. The maker of this app bears no responsibility for any damage that the use of this software may cause.");
+        }
+
+        private ImageCodecInfo GetEncoder(ImageFormat format)
+        {
+            ImageCodecInfo[] codecs = ImageCodecInfo.GetImageDecoders();
+            foreach (ImageCodecInfo codec in codecs)
+            {
+                if (codec.FormatID == format.Guid)
+                {
+                    return codec;
+                }
+            }
+            return null;
+        }
+    }
+}
