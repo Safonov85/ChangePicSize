b77cc01e3e7e5b2f0ae6ef8c07f0d44f39878589
safonov <safonov1985@protonmail.com>
2020-10-02 22:42:59 +0200

    Default Settings class with properties added

---
+++ ChangePicSize/SettingsChangePic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangePicSize
+{
+    public class SettingsChangePic
+    {
+        public int Resolution { get; set; }
+        public int Quality { get; set; }
+        public string FileType { get; set; }
+        public bool Rotate90Right { get; set; }
+        public string SavePath { get; set; }
+
+    }
+}
